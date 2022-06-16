594dbacfe35e7e949a9e4f5aadb971f45f7f4e1d
Maks <mikastricyn@outlook.com>
2022-06-16 07:24:55 +0300

    init Spring commit

---
+++ src/main/java/ru/kastricyn/alfatest/AlfaTestApplication.java
@@ -0,0 +1,12 @@
+package ru.kastricyn.alfatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlfaTestApplication.class, args);
+  }
+}
---
+++ src/test/java/ru/kastricyn/alfatest/AlfaTestApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.kastricyn.alfatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaTestApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
